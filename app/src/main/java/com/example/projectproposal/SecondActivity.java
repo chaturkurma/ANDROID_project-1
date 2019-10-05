b3a6ff5c79fee1e065f31a21a4ee3445b83c8aac
chaturkurma <chaturk31@gmail.com>
2019-10-04 20:58:18 -0500

    Adding onclick activity for universities

--- app/src/main/java/com/example/projectproposal/SecondActivity.java
+++ app/src/main/java/com/example/projectproposal/SecondActivity.java
@@ -2,7 +2,11 @@ package com.example.projectproposal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +14,33 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        TextView tv = (TextView) findViewById(R.id.univ1);
+        TextView tv1 = (TextView) findViewById(R.id.univ2);
+
+
+        tv.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Intent ini = new Intent(SecondActivity.this,ThirdActivity.class);
+                startActivity(ini);
+
+            }
+
+        });
+
+        tv1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Intent ini = new Intent(SecondActivity.this,fourthActivity.class);
+                startActivity(ini);
+
+            }
+
+        });
+
     }
 }
