45fe6e692264f229cdbdc750fe7c21b635609704
rajesh4322 <rajesh.tarigopula96@gmail.com>
2019-10-25 22:17:03 -0500

    commit changes for ForgotPassword.java, activity_forgot_password.xml

--- app/src/main/java/com/example/projectproposal/ForgotPassword.java
+++ app/src/main/java/com/example/projectproposal/ForgotPassword.java
@@ -4,10 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.RequestPasswordResetCallback;
 
 public class ForgotPassword extends AppCompatActivity {
     EditText edEmail;
@@ -27,10 +33,10 @@ public class ForgotPassword extends AppCompatActivity {
                     public void done(ParseException e) {
                         if (e == null) {
                             // An email was successfully sent with reset instructions.
-                            Toast.makeText(ResetPasswordActivity.this, "An email was successfully sent with reset instructions.", Toast.LENGTH_LONG).show();
+                            Toast.makeText(ForgotPassword.this, "An email was successfully sent with reset instructions.", Toast.LENGTH_LONG).show();
                         } else {
                             // Something went wrong. Look at the ParseException to see what's up.
-                            Toast.makeText(ResetPasswordActivity.this, "Something went wrong. Look at the ParseException to see what's up.", Toast.LENGTH_LONG).show();
+                            Toast.makeText(ForgotPassword.this, "Something went wrong. Look at the ParseException to see what's up.", Toast.LENGTH_LONG).show();
                         }
                     }
                 });
