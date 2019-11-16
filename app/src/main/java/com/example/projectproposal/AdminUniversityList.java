13734624c27068caa3eed8536a1af9344b3f2095
chaturkurma <chaturk31@gmail.com>
2019-11-15 21:52:15 -0600

    admin university list

---
+++ app/src/main/java/com/example/projectproposal/AdminUniversityList.java
@@ -0,0 +1,14 @@
+package com.example.projectproposal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminUniversityList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_university_list);
+    }
+}
